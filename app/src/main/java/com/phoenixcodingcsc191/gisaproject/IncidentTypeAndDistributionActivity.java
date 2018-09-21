e3a9782ad37d3cc9182e7dc21e032481aa2c7325
Aaron Le <aaronle123@gmail.com>
2018-09-21 12:57:42 -0700

    German's Pages

---
+++ app/src/main/java/com/phoenixcodingcsc191/gisaproject/IncidentTypeAndDistributionActivity.java
@@ -0,0 +1,13 @@
+package com.phoenixcodingcsc191.gisaproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class IncidentTypeAndDistributionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_measurement_screen);
+    }
+}
