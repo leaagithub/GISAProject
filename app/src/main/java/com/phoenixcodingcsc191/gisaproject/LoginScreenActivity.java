d50b55232023a8ca1e0555dc7fcaa3e969fa03b5
Aaron Le <ale2pwn@gmail.com>
2018-10-01 01:03:53 -0700

    Login and Signup Verification

--- app/src/main/java/com/phoenixcodingcsc191/gisaproject/LoginScreenActivity.java
+++ app/src/main/java/com/phoenixcodingcsc191/gisaproject/LoginScreenActivity.java
@@ -2,23 +2,32 @@ package com.phoenixcodingcsc191.gisaproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginScreenActivity extends AppCompatActivity {
+
+    private TextInputLayout textInputUsername;
+    private TextInputLayout textInputPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-        signUpButonActivity();
-        loginButtonActivity();
+        textInputUsername = findViewById(R.id.textInputUser);
+        textInputPassword = findViewById(R.id.textInputPassword);
+
+        signUpButtonActivity();
+        //
     }
     @Override public void onBackPressed(){}
 
-    private void signUpButonActivity(){
+    private void signUpButtonActivity(){
         Button signupButton = (Button) findViewById(R.id.buttonSignUp);
         signupButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -37,5 +46,46 @@ public class LoginScreenActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private boolean validateUser(){
+        String usernameInput = textInputUsername.getEditText().getText().toString().trim();
+
+        if(usernameInput.isEmpty()){
+            textInputUsername.setError("Username Cannot be Empty");
+            return false;
+        }else if (usernameInput.length() > 15) {
+            textInputUsername.setError("Username too long");
+            return false;
+        }else{
+            textInputUsername.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePassword(){
+        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
+
+        if(passwordInput.isEmpty()){
+            textInputPassword.setError("Password Cannot be Empty");
+            return false;
+        }else{
+            textInputPassword.setError(null);
+            return true;
+        }
+    }
+    public void confirmInput(View v){
+        if (!validateUser() | !validatePassword()){
+            return;
+        }
+
+        String input = "Username: "+ textInputUsername.getEditText().getText().toString();
+        input += "\n";
+        input += "Password: "+ textInputPassword.getEditText().getText().toString();
+
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+        //loginButtonActivity();
+        startActivity(new Intent(LoginScreenActivity.this, IncidentOptionsActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/phoenixcodingcsc191/gisaproject/SignUpActivity.java
+++ app/src/main/java/com/phoenixcodingcsc191/gisaproject/SignUpActivity.java
@@ -1,18 +1,34 @@
 package com.phoenixcodingcsc191.gisaproject;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private TextInputLayout textInputFirstName;
+    private TextInputLayout textInputLastName;
+    private TextInputLayout textInputPassword;
+    private TextInputLayout textInputPhoneNo;
+    private TextInputLayout textEmployeeID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        signUpButonActivity();
+
+        textInputFirstName = findViewById(R.id.textInputFirstNameSignUp);
+        textInputLastName = findViewById(R.id.textInputLastNameSignUp);
+        textInputPassword = findViewById(R.id.textInputPasswordSignUp);
+        textInputPhoneNo = findViewById(R.id.textInputPhoneNoSignUp);
+        textEmployeeID = findViewById(R.id.textInputEmployeeIDSignUp);
+
+        //signUpButonActivity();
     }
     @Override public void onBackPressed(){}
     private void signUpButonActivity(){
@@ -25,4 +41,70 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
     }
+
+    private boolean validateFirstName(){
+        String firstNameInput = textInputFirstName.getEditText().getText().toString().trim();
+
+        if(firstNameInput.isEmpty()){
+            textInputFirstName.setError("First Name Cannot be Empty");
+            return false;
+        }else{
+            textInputFirstName.setError(null);
+            return true;
+        }
+    }
+    private boolean validateLastName(){
+        String lastNameInput = textInputLastName.getEditText().getText().toString().trim();
+
+        if(lastNameInput.isEmpty()){
+            textInputLastName.setError("Last Name Cannot be Empty");
+            return false;
+        }else{
+            textInputLastName.setError(null);
+            return true;
+        }
+    }
+    private boolean validatePassword(){
+        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
+
+        if(passwordInput.isEmpty()){
+            textInputPassword.setError("Password Cannot be Empty");
+            return false;
+        }else{
+            textInputPassword.setError(null);
+            return true;
+        }
+    }
+    private boolean validateEmployeeID(){
+        String employeeIDInput = textEmployeeID.getEditText().getText().toString().trim();
+
+        if(employeeIDInput.isEmpty()){
+            textEmployeeID.setError("Employee ID Cannot be Empty");
+            return false;
+        }else{
+            textEmployeeID.setError(null);
+            return true;
+        }
+    }
+    private boolean validatePhoneNo(){
+        String phoneNoInput = textInputPhoneNo.getEditText().getText().toString().trim();
+
+        if(phoneNoInput.isEmpty()){
+            textInputPhoneNo.setError("Phone Number Cannot be Empty");
+            return false;
+        }else{
+            textInputPhoneNo.setError(null);
+            return true;
+        }
+    }
+
+    public void confirmInput(View v){
+        if (!validateFirstName() | !validateLastName() | !validateEmployeeID() | !validatePhoneNo() | !validatePassword()){
+            return;
+        }
+        //loginButtonActivity();
+        startActivity(new Intent(SignUpActivity.this, LoginScreenActivity.class));
+        finish();
+    }
+
 }
