9b2db19226c87cc5e9725121f0ed8624978605ba
leaagithub <aaronle123@gmail.com>
2018-11-24 22:21:32 +0000

    Testbranch2

---
+++ app/src/main/java/com/phoenixcodingcsc191/gisaproject/Retrofit/RetrofitClient.java
@@ -0,0 +1,22 @@
+package com.phoenixcodingcsc191.gisaproject.Retrofit;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit instance;
+    //192.168.29.111:3000 my cpu local address
+    //http://10.0.2.2:3000/ android emulator node server
+    //http://100.115.92.205:3000/ chromebook node server
+    public static final String API_BASE_URL = "http://100.115.92.205:3000/";
+    public static Retrofit getInstance(){
+        if (instance == null)
+            instance = new Retrofit.Builder()
+                    .baseUrl(API_BASE_URL)
+                    .addConverterFactory(ScalarsConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
+        return instance;
+    }
+}
