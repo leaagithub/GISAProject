867974dfbafcb287d437c0aacf9444de07993dba
Aaron Le <ale2pwn@gmail.com>
2018-11-29 18:53:33 -0800

    SITE INFORMATION IS SAVED....DONE NEW INCIDENT REPORT WORKS AS INTENDED SAVED INCIDENT REPORT WORKS AS INTENDED

--- app/src/main/java/com/phoenixcodingcsc191/gisaproject/IncidentOptionsActivity.java
+++ app/src/main/java/com/phoenixcodingcsc191/gisaproject/IncidentOptionsActivity.java
@@ -1,23 +1,50 @@
 package com.phoenixcodingcsc191.gisaproject;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class IncidentOptionsActivity extends AppCompatActivity {
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incident_options);
         newIncidentButtonActivity();
+        resumeIncidentButtonActivity();
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreferences.edit();
     }
     @Override public void onBackPressed(){}
 
     private void newIncidentButtonActivity(){
         Button newIncidentButton = (Button) findViewById(R.id.buttonNewIncident);
+        newIncidentButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mEditor.putString(getString(R.string.SiteInfoDate),"");
+                mEditor.putString(getString(R.string.SiteInfoDistrict),"");
+                mEditor.putString(getString(R.string.SiteInfoCountry),"");
+                mEditor.putString(getString(R.string.SiteInfoRoute),"");
+                mEditor.putString(getString(R.string.SiteInfoPostMile),"");
+                mEditor.putString(getString(R.string.SiteInfoDateIncidentReport),"");
+                mEditor.putString(getString(R.string.SiteInfoLatitude),"");
+                mEditor.putString(getString(R.string.SiteInfoLongitude),"");
+                mEditor.commit();
+                startActivity(new Intent(IncidentOptionsActivity.this, SiteInformationActivity.class));
+                finish();
+            }
+        });
+    }
+    private void resumeIncidentButtonActivity(){
+        Button newIncidentButton = (Button) findViewById(R.id.buttonResumeIncidentOptionPage);
         newIncidentButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
--- app/src/main/java/com/phoenixcodingcsc191/gisaproject/SiteInformationActivity.java
+++ app/src/main/java/com/phoenixcodingcsc191/gisaproject/SiteInformationActivity.java
@@ -1,17 +1,48 @@
 package com.phoenixcodingcsc191.gisaproject;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class SiteInformationActivity extends AppCompatActivity {
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
+
+    private TextInputLayout textInputDate;
+    private TextInputLayout textInputDistrict;
+    private TextInputLayout textInputCountry;
+    private TextInputLayout textInputRoute;
+    private TextInputLayout textInputPostMile;
+    private TextInputLayout textInputDateIncReport;
+    private TextInputLayout textInputLatitude;
+    private TextInputLayout textInputLongitude;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_siteinformation);
+
+        textInputDate = findViewById(R.id.textInputDateSiteInfo);
+        textInputDistrict = findViewById(R.id.textInputDistrictSiteInfo);
+        textInputCountry = findViewById(R.id.textInputCountrySiteInfo);
+        textInputRoute = findViewById(R.id.textInputRouteSiteInfo);
+        textInputPostMile = findViewById(R.id.textInputPostMileSiteInfo);
+        textInputDateIncReport = findViewById(R.id.textInputDateIncReportSiteInfo);
+        textInputLatitude = findViewById(R.id.textInputLatitudeSiteInfo);
+        textInputLongitude = findViewById(R.id.textInputLongitudeSiteInfo);
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreferences.edit();
+
+        checkSharedPrefernce();
         nextPageButton();
         saveAndExitButtonActivity();
     }
@@ -23,6 +54,16 @@ public class SiteInformationActivity extends AppCompatActivity {
             @Override
             
             public void onClick(View view){
+                mEditor.putString(getString(R.string.SiteInfoDate),textInputDate.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoDistrict),textInputDistrict.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoCountry),textInputCountry.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoRoute),textInputRoute.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoPostMile),textInputPostMile.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoDateIncidentReport),textInputDateIncReport.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoLatitude),textInputLatitude.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoLongitude),textInputLongitude.getEditText().getText().toString());
+
+                mEditor.commit();
                 startActivity(new Intent(SiteInformationActivity.this, IncidentTypeAndDistributionActivity.class));
                 finish();
             }
@@ -33,9 +74,37 @@ public class SiteInformationActivity extends AppCompatActivity {
         saveAndExitButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                mEditor.putString(getString(R.string.SiteInfoDate),textInputDate.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoDistrict),textInputDistrict.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoCountry),textInputCountry.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoRoute),textInputRoute.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoPostMile),textInputPostMile.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoDateIncidentReport),textInputDateIncReport.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoLatitude),textInputLatitude.getEditText().getText().toString());
+                mEditor.putString(getString(R.string.SiteInfoLongitude),textInputLongitude.getEditText().getText().toString());
+                mEditor.commit();
                 startActivity(new Intent(SiteInformationActivity.this, IncidentOptionsActivity.class));
                 finish();
             }
         });
+    }
+    private void checkSharedPrefernce(){
+        String Date = mPreferences.getString(getString(R.string.SiteInfoDate),"");
+        String District = mPreferences.getString(getString(R.string.SiteInfoDistrict),"");
+        String Country = mPreferences.getString(getString(R.string.SiteInfoCountry),"");
+        String Route = mPreferences.getString(getString(R.string.SiteInfoRoute),"");
+        String PostMile = mPreferences.getString(getString(R.string.SiteInfoPostMile),"");
+        String DateIncidentReport = mPreferences.getString(getString(R.string.SiteInfoDateIncidentReport),"");
+        String Latitude = mPreferences.getString(getString(R.string.SiteInfoLatitude),"");
+        String Longitude = mPreferences.getString(getString(R.string.SiteInfoLongitude),"");
+
+        textInputDate.getEditText().setText(Date);
+        textInputDistrict.getEditText().setText(District);
+        textInputCountry.getEditText().setText(Country);
+        textInputRoute.getEditText().setText(Route);
+        textInputPostMile.getEditText().setText(PostMile);
+        textInputDateIncReport.getEditText().setText(DateIncidentReport);
+        textInputLatitude.getEditText().setText(Latitude);
+        textInputLongitude.getEditText().setText(Longitude);
     }
 }
