7acf390a91f6e5b4cc4d4237b664dda1392c62d5
Aaron Le <ale2pwn@gmail.com>
2018-11-30 01:21:13 -0800

    MEASUREMENT SCREEN.....Done

--- app/src/main/java/com/phoenixcodingcsc191/gisaproject/MeasurementScreenActivity.java
+++ app/src/main/java/com/phoenixcodingcsc191/gisaproject/MeasurementScreenActivity.java
@@ -1,17 +1,54 @@
 package com.phoenixcodingcsc191.gisaproject;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
 
 public class MeasurementScreenActivity extends AppCompatActivity {
+
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
+
+    private TextInputLayout textInputLSLength;
+    private TextInputLayout textInputMSHeightt;
+    private TextInputLayout textInputLSSlope;
+    private TextInputLayout textInputRoadwayLength;
+    private TextInputLayout textInputRoadwayWidth;
+    private TextInputLayout textInputLSWidth;
+    private TextInputLayout textInputSlopeHeight;
+    private TextInputLayout textInputOriginalSlope;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measurement_screen);
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreferences.edit();
+        textInputLSLength = findViewById(R.id.textInputLandSlideLength);
+        textInputOriginalSlope = findViewById(R.id.textInputOriginalSlope);
+        textInputSlopeHeight = findViewById(R.id.textInputSlopeHeight);
+        textInputMSHeightt = findViewById(R.id.textInputMainScrapHeight);
+        textInputLSWidth = findViewById(R.id.textInputLandSlideWidth);
+        textInputRoadwayLength = findViewById(R.id.textInputLengthOfRoadwayEnroached);
+        textInputRoadwayWidth = findViewById(R.id.textInputWidthOfRoadwayEnroached);
+        textInputLSSlope = findViewById(R.id.textInputLandSlideSlope);
+
+        savedInformation();
         saveAndExitButtonActivity();
         previousButtonActivity();
         nextPageButtonActivity();
@@ -24,15 +61,69 @@ public class MeasurementScreenActivity extends AppCompatActivity {
         helpButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-
+                showImage();
             }
         });
+    }
+    private void pushToString(){
+        mEditor.putString(getString(R.string.MeasurementOriginalSlope),textInputOriginalSlope.getEditText().getText().toString());
+        mEditor.putString(getString(R.string.MeasurementSlopeHeight),textInputSlopeHeight.getEditText().getText().toString());
+        mEditor.putString(getString(R.string.MeasurementLandslideSlope),textInputLSSlope.getEditText().getText().toString());
+        mEditor.putString(getString(R.string.MeasurementWidthOfRoadwayEncroached),textInputRoadwayWidth.getEditText().getText().toString());
+        mEditor.putString(getString(R.string.MeasurementLengthOfRoadwayEncroached),textInputRoadwayLength.getEditText().getText().toString());
+        mEditor.putString(getString(R.string.MeasurementMainscrapHeight),textInputMSHeightt.getEditText().getText().toString());
+        mEditor.putString(getString(R.string.MeasurementLandslideLength),textInputLSLength.getEditText().getText().toString());
+        mEditor.putString(getString(R.string.MeasurementLandslideWidth),textInputLSWidth.getEditText().getText().toString());
+        mEditor.commit();
+    }
+    private void savedInformation(){
+        String MeasurementOriginalSlopeSaved = mPreferences.getString(getString(R.string.MeasurementOriginalSlope),"");
+        String MeasurementSlopeHeightSaved = mPreferences.getString(getString(R.string.MeasurementSlopeHeight),"");
+        String MeasurementLandslideSlopeSaved = mPreferences.getString(getString(R.string.MeasurementLandslideSlope),"");
+        String MeasurementWidthOfRoadwayEncroachedSaved = mPreferences.getString(getString(R.string.MeasurementWidthOfRoadwayEncroached),"");
+        String MeasurementLengthOfRoadwayEncroachedSaved = mPreferences.getString(getString(R.string.MeasurementLengthOfRoadwayEncroached),"");
+        String MeasurementMainscrapHeightSaved = mPreferences.getString(getString(R.string.MeasurementMainscrapHeight),"");
+        String MeasurementLandslideLengthSaved = mPreferences.getString(getString(R.string.MeasurementLandslideLength),"");
+        String MeasurementLandslideWidthSaved = mPreferences.getString(getString(R.string.MeasurementLandslideWidth),"");
+
+        textInputOriginalSlope.getEditText().setText(MeasurementOriginalSlopeSaved);
+        textInputSlopeHeight.getEditText().setText(MeasurementSlopeHeightSaved);
+        textInputLSSlope.getEditText().setText(MeasurementLandslideSlopeSaved);
+        textInputRoadwayWidth.getEditText().setText(MeasurementWidthOfRoadwayEncroachedSaved);
+        textInputRoadwayLength.getEditText().setText(MeasurementLengthOfRoadwayEncroachedSaved);
+        textInputMSHeightt.getEditText().setText(MeasurementMainscrapHeightSaved);
+        textInputLSLength.getEditText().setText(MeasurementLandslideLengthSaved);
+        textInputLSWidth.getEditText().setText(MeasurementLandslideWidthSaved);
+    }
+    private void showImage(){
+
+        Dialog builder = new Dialog(this);
+        builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        builder.getWindow().setBackgroundDrawable(
+                new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+                //nothing;
+            }
+        });
+
+        ImageView imageView = new ImageView(this);
+        Uri imageUri = Uri.parse("android.resource://com.phoenixcodingcsc191.gisaproject/"+R.drawable.exdrawing);
+        imageView.setImageURI(null);
+        imageView.setImageURI(imageUri);
+        builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        builder.show();
+
     }
     private void saveAndExitButtonActivity(){
         Button saveAndExitButton = (Button) findViewById(R.id.buttonSaveAndExitMeasurementPage);
         saveAndExitButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                pushToString();
                 startActivity(new Intent(MeasurementScreenActivity.this, IncidentOptionsActivity.class));
                 finish();
             }
@@ -43,6 +134,7 @@ public class MeasurementScreenActivity extends AppCompatActivity {
         previousButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                pushToString();
                 startActivity(new Intent(MeasurementScreenActivity.this, IncidentTypeAndDistributionActivity.class));
                 finish();
             }
@@ -53,6 +145,7 @@ public class MeasurementScreenActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                pushToString();
                 startActivity(new Intent(MeasurementScreenActivity.this, SketchActivity.class));
                 finish();
             }
