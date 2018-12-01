e14ae256a9de93fe2523c19fd8c1312b47677c34
Aaron Le <ale2pwn@gmail.com>
2018-12-01 12:57:35 -0800

    Observation Screen ...... Done Recommended Actions: Strings.xml setup Field ID has been fixed appropriately

--- app/src/main/java/com/phoenixcodingcsc191/gisaproject/ObservationScreenActivity.java
+++ app/src/main/java/com/phoenixcodingcsc191/gisaproject/ObservationScreenActivity.java
@@ -1,29 +1,55 @@
 package com.phoenixcodingcsc191.gisaproject;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Observable;
 
 public class ObservationScreenActivity extends AppCompatActivity {
+
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
+
+    private EditText ObservationNotesET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_observation_screen);
+
+        ObservationNotesET = findViewById(R.id.EditTextNotesObservation);
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreferences.edit();
+
+        loadSaved();
         previousButton();
         nextButtonActivity();
         saveAndExitActivity();
     }
 
     @Override public void onBackPressed(){}
+    private void savePref(){
+        mEditor.putString(getString(R.string.ObservationNotes),ObservationNotesET.getText().toString());
+        mEditor.commit();
+    }
 
+    private void loadSaved(){
+        String ObservationNotes = mPreferences.getString(getString(R.string.ObservationNotes),"");
+        ObservationNotesET.setText(ObservationNotes);
+    }
     private void previousButton(){
         Button previous = (Button) findViewById(R.id.buttonPreviousObservation);
         previous.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                savePref();
                 startActivity(new Intent(ObservationScreenActivity.this, SketchActivity.class));
                 finish();
             }
@@ -34,6 +60,7 @@ public class ObservationScreenActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                savePref();
                 startActivity(new Intent(ObservationScreenActivity.this, RecommendedAction.class));
                 finish();
             }
@@ -44,6 +71,7 @@ public class ObservationScreenActivity extends AppCompatActivity {
         saveAndExit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                savePref();
                 startActivity(new Intent(ObservationScreenActivity.this, IncidentOptionsActivity.class));
                 finish();
             }
--- app/src/main/java/com/phoenixcodingcsc191/gisaproject/Retrofit/RetrofitClient.java
+++ app/src/main/java/com/phoenixcodingcsc191/gisaproject/Retrofit/RetrofitClient.java
@@ -6,10 +6,10 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit instance;
-    //192.168.29.111:3000 my cpu local address
+    //http://192.168.29.111:3000/ my cpu local address
     //http://10.0.2.2:3000/ android emulator local server
     //http://100.115.92.205:3000/ chromebook local server
-    public static final String API_BASE_URL = "http://100.115.92.205:3000/";
+    public static final String API_BASE_URL = "http://192.168.29.111:3000/";
     public static Retrofit getInstance(){
         if (instance == null)
             instance = new Retrofit.Builder()
